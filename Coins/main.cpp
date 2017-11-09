1edbca26e724aff6b1229f397e770561f612f580
maxqlord <lordmaxq@gmail.com>
2017-11-09 08:45:34 -0500

    Added displayimage test project

---
+++ Coins/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+
+/*
+1. Smooth with Gaussian filter
+2. Identify edge pixels with using Sobel kernel and Canny algorithm
+3. Use gradient data to find circles using Hough Transform
+ */
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
