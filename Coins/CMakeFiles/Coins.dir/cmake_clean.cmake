f9884c3e48f8a095abb7a82452cf2b3e479d91ca
Max Lord <maxlord@Maxs-MacBook-Pro.local>
2017-11-16 13:20:48 -0500

    Expanded Canny code

---
+++ Coins/CMakeFiles/Coins.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Coins.dir/main.cpp.o"
+  "Coins.pdb"
+  "Coins"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Coins.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- Coins/main.cpp
+++ Coins/main.cpp
@@ -1,13 +1,51 @@
-#include <iostream>
+#include <stdio.h>
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
 
 
-/*
-1. Smooth with Gaussian filter
-2. Identify edge pixels with using Sobel kernel and Canny algorithm
-3. Use gradient data to find circles using Hough Transform
- */
+Mat edgeDetect(Mat src, int upperThreshold, int lowerThreshold, double size = 3) //thresholds are for derivative calcs
+{
+    return src;
+}
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+Mat gaussblur(Mat src, Mat dst, Size ksize, double sigmaX, double sigmaY) {
+    GaussianBlur(src, dst, ksize, sigmaX, sigmaY);
+    return dst;
+}
+
+Mat grayscale(Mat src, Mat gray) {
+    cvtColor(src, gray, CV_BGR2GRAY);
+    return gray;
+}
+
+int main(int argc, char** argv )
+{
+    if ( argc != 2 ) //if command line arguments aren't correct
+    {
+        printf("usage: DisplayImage.out <Image_Path>\n");
+        return -1;
+    }
+
+    Mat image, gray, blur; //declare image matrix and destination for grayscale matrix and blur matrix
+    image = imread( argv[1], 1 ); //read image into image matrix
+    if ( !image.data ) //if image is empty/does not exist
+    {
+        printf("No image data \n");
+        return -1;
+    }
+
+    gray = grayscale(image, gray); //convert image to grayscale and save into gray matrix
+    blur = gaussblur(gray, blur, Size(3,3), 0, 0); //apply gaussian blur to grayscale matrix and save into blur matrix
+
+
+    namedWindow("blur+gray", CV_WINDOW_NORMAL);
+    imshow("blur+gray", blur);
+    waitKey(0);
+
     return 0;
+
 }
