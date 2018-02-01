3dd550ede38dac1a700ccf4367a53f2bac1c986d
maxqlord <lordmaxq@gmail.com>
2018-02-01 13:11:09 -0500

    Added dollar value counting to coins opencv

--- CoinsOpenCV/main.cpp
+++ CoinsOpenCV/main.cpp
@@ -32,6 +32,12 @@ vector<Vec3f> houghTransform(Mat src, vector<Vec3f> circles) {
 double countMoney(vector<Vec3f> circles, Mat img) {
     double counter = 0;
     vector<double> radii;
+    double dimeVal = 100;
+    double pennyVal = 120;
+    double nickelVal = 140;
+    double quarterVal = 180;
+    double dollarVal = 200;
+    double halfVal = 300;
 
     for( int i = 0; i < circles.size(); i++ ) {
         Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
@@ -41,6 +47,21 @@ double countMoney(vector<Vec3f> circles, Mat img) {
         circle( img, center, 3, Scalar(0,255,0), -1, 8, 0 );
         // draw the circle outline
         circle( img, center, radius, Scalar(0,0,255), 3, 8, 0 );
+
+        if(radius < dimeVal) {
+            counter += .1;
+        } else if(radius < pennyVal) {
+            counter += .01;
+        } else if(radius < nickelVal) {
+            counter += .05;
+        } else if(radius < quarterVal) {
+            counter += .25;
+        } else if(radius < dollarVal) {
+            counter += 1;
+        } else if(radius < halfVal) {
+            counter += .5;
+        }
+
     }
 
         /*
