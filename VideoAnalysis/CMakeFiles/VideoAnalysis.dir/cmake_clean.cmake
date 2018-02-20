126039134d74c749d3f97ef9461aea0dd875c286
maxqlord <lordmaxq@gmail.com>
2018-02-20 14:06:49 -0500

    Added video

---
+++ VideoAnalysis/CMakeFiles/VideoAnalysis.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VideoAnalysis.dir/main.cpp.o"
+  "VideoAnalysis.pdb"
+  "VideoAnalysis"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VideoAnalysis.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
