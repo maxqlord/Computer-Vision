9f5b0c08c8e84fc26fdbb9e2f7c90cf4a4ef2318
maxqlord <lordmaxq@gmail.com>
2018-02-20 13:26:29 -0500

    Created Video Analysis project

---
+++ CoinsOpenCV/CMakeFiles/CoinsOpenCV.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CoinsOpenCV.dir/main.cpp.o"
+  "CoinsOpenCV.pdb"
+  "CoinsOpenCV"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CoinsOpenCV.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ VideoAnalysis/cmake-build-debug/CMakeFiles/VideoAnalysis.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for VideoAnalysis.
+# This may be replaced when dependencies are built.
