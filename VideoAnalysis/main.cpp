375aa58ab756c1b17e6286a25858eb29bcd0695f
maxqlord <lordmaxq@gmail.com>
2018-03-01 13:42:59 -0500

    Found chessboard and printing interior corners

--- VideoAnalysis/main.cpp
+++ VideoAnalysis/main.cpp
@@ -1,45 +1,77 @@
-#include <cv.h>
-#include <highgui.h>
+#include "opencv2/opencv.hpp"
 #include <iostream>
-#include <opencv2/opencv.hpp>
 
 using namespace std;
 using namespace cv;
 
+void print_vec(vector<Point2f> corners) {
+    for(int i = 0; i < corners.size(); i++) {
+        printf("X: %f\t Y: %f\n", corners[i].x, corners[i].y);
+    }
+}
 
-int main() {
-
-    Mat frame, gray;
+int main(){
 
     // Create a VideoCapture object and open the input file
     // If the input is the web camera, pass 0 instead of the video file name
-    VideoCapture cap("StructureFromMotionVideo.mp4");
+    //VideoCapture cap("StructureFromMotionVideo.mp4");
+    VideoCapture cap("withChessBoard.MOV");
 
     // Check if camera opened successfully
-    if (!cap.isOpened()) {
+    if(!cap.isOpened()){
         cout << "Error opening video stream or file" << endl;
         return -1;
     }
-    cap >> frame;
-    while(!frame.empty()) {
-        Size patternsize(8, 6); //interior number of corners
-        cvtColor(frame, gray, CV_BGR2GRAY);
+
+    while(true){
+
+        Mat frame, frameGray;
+        Size patternsize(7,7);
         vector<Point2f> corners;
-        bool patternfound = findChessboardCorners(gray, patternsize, corners,
-                                                  CALIB_CB_ADAPTIVE_THRESH + CALIB_CB_NORMALIZE_IMAGE
-                                                  + CALIB_CB_FAST_CHECK);
-        if (patternfound) {
-            printf("found");
-            cornerSubPix(gray, corners, Size(11, 11), Size(-1, -1),
-                         TermCriteria(CV_TERMCRIT_EPS + CV_TERMCRIT_ITER, 30, 0.1));
+        // Capture frame-by-frame
+        cap >> frame;
+        if (frame.empty()) {
+            printf("empty\n");
+            break;
         }
+        //MOV coming in flipped both ways
+        flip(frame, frame, 0);
+        flip(frame, frame, +1);
+        cvtColor(frame, frameGray, CV_BGR2GRAY);
+
+        // If the frame is empty, break immediately
+
+
+        bool patternfound = findChessboardCorners(frameGray, patternsize, corners,
+                                                  CALIB_CB_ADAPTIVE_THRESH + CALIB_CB_NORMALIZE_IMAGE);
+
+
+        if(patternfound) {
+            //printf("found\n");
+            cornerSubPix(frameGray, corners, Size(11, 11), Size(-1, -1),
+                         TermCriteria(CV_TERMCRIT_EPS + CV_TERMCRIT_ITER, 30, 0.1));
+        } /*else {
+            printf("not found\n");
+        }*/
+
+
         drawChessboardCorners(frame, patternsize, Mat(corners), patternfound);
+
+        // Display the resulting frame
+        imshow( "Frame", frame );
+        print_vec(corners);
+
+        // Press  ESC on keyboard to exit
+        waitKey(5000);
+
 
     }
 
+    // When everything done, release the video capture object
     cap.release();
+
+    // Closes all the frames
     destroyAllWindows();
 
     return 0;
-
 }
